6ed6e3d78db421385667d8b13c0b4fb8d9416a9a
asyasaeed <72996931+asyasaeed@users.noreply.github.com>
2021-03-15 21:27:50 -0400

    19.2

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,6 @@
 // Create a variable below
+var intro="Hello World";
 
 // Console.log() your variable here.
+console.log(intro);
+alert(intro);
--- 19.1-activities/02_More_Variables/unsolved/js/index.js
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,23 @@
 // Create the addNumbers function here.
 
+function dosomething(){
+    var two = 2;
+    var three = 3;
+    var five = two + three;
+
+   
+   
+    console.log(five);
+    alert(five);
+    }
+    dosomething();
+
 // Create the stringConcat function here.
+function stringcat(){
+taco="taco";
+cat="cat";
+tacocat="taco"+" "+"cat";
+console.log(tacocat);
+alert(tacocat);
+}
+stringcat();
